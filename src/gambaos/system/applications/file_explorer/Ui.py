dbcce12696d5a193f6f31c9ac912c64d93605cda
sverryboiii <sverreplaysgoodbs@gmail.com>
2026-09-14 08:57:12 +0200

    [feat] Start on file explorer.

---
+++ src/gambaos/system/applications/file_explorer/Ui.py
@@ -0,0 +1,5 @@
+from src.gambaos.system.applications.file_explorer import Functions
+import sverpykit as spk
+
+def add_layers():
+    pass
---
+++ src/gambaos/system/applications/file_explorer/main.py
@@ -0,0 +1,4 @@
+from src.gambaos.system.applications.file_explorer import Ui
+
+def main():
+    Ui.add_layers()
