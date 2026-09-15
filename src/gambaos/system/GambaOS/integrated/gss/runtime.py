be9745bad990410ca401964fdc4b90a45edfd588
sverryboiii <sverreplaysgoodbs@gmail.com>
2026-09-15 18:37:24 +0200

    [feat] Added GSS (Gamba Style Sheets).

---
+++ src/gambaos/system/GambaOS/integrated/gss/runtime.py
@@ -0,0 +1,9 @@
+from src.gambaos.system.GambaOS.integrated.gss import parser
+import sverpykit as spk
+
+def execute(code, window: spk.Window):
+
+    parsed_code = parser.parse(code)
+
+    if parsed_code.get("background-color", False):
+        window.color = parsed_code["background-color"]
